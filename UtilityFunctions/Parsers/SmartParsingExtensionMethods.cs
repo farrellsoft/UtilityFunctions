b171e75257447807883cb12e11ad2e1321f6dfd7
Jason Farrell <imjason@gmail.com>
2025-05-04 12:09:45 -0400

    reorganized, added some unit tests

---
+++ UtilityFunctions/Parsers/SmartParsingExtensionMethods.cs
@@ -0,0 +1,46 @@
+
+namespace Farrellsoft.UtilityFunctions.Parsers
+{
+    public static class SmartParsingExtensionMethods
+    {
+        public static int AsInt(this string str, int defaultValue = int.MinValue)
+        {
+            if (int.TryParse(str, out int result))
+            {
+                return result;
+            }
+
+            return defaultValue;
+        }
+
+        public static long AsLong(this string str, long defaultValue = long.MinValue)
+        {
+            if (long.TryParse(str, out long result))
+            {
+                return result;
+            }
+
+            return defaultValue;
+        }
+
+        public static float AsFloat(this string str, float defaultValue = float.MinValue)
+        {
+            if (float.TryParse(str, out float result))
+            {
+                return result;
+            }
+
+            return defaultValue;
+        }
+
+        public static double AsDouble(this string str, double defaultValue = double.MinValue)
+        {
+            if (double.TryParse(str, out double result))
+            {
+                return result;
+            }
+
+            return defaultValue;
+        }
+    }
+}
