1d78f82daf5d136cc667b8d0eb9f88db8d846045
Jason Farrell <imjason@gmail.com>
2025-05-04 22:26:26 -0400

    modifications to set functions

--- UtilityFunctions/Sets/SetExtensionMethods.cs
+++ UtilityFunctions/Sets/SetExtensionMethods.cs
@@ -10,18 +10,21 @@ namespace Farrellsoft.UtilityFunctions
         public static IEnumerable<T> SymmetricDifference<T>(this IEnumerable<T> source, IEnumerable<T> other, IEqualityComparer<T> comparer = null)
         {
             if (source == null) throw new ArgumentNullException(nameof(source));
+            var sourceList = source.ToList();
+            
             if (other == null) throw new ArgumentNullException(nameof(other));
+            var otherList = other.ToList();
 
-            foreach (var item in source)
+            foreach (var item in sourceList)
             {
-                if (!other.Contains(item, comparer ?? EqualityComparer<T>.Default))
+                if (!otherList.Contains(item, comparer ?? EqualityComparer<T>.Default))
                 {
                     yield return item;
                 }
             }
-            foreach (var item in other)
+            foreach (var item in otherList)
             {
-                if (!source.Contains(item, comparer ?? EqualityComparer<T>.Default))
+                if (!sourceList.Contains(item, comparer ?? EqualityComparer<T>.Default))
                 {
                     yield return item;
                 }
@@ -31,9 +34,11 @@ namespace Farrellsoft.UtilityFunctions
         public static IEnumerable<T> Map<T>(this IEnumerable<T> source, Action<T> mapFunc)
         {
             if (source == null) throw new ArgumentNullException(nameof(source));
+            var sourceList = source.ToList();
+            
             if (mapFunc == null) throw new ArgumentNullException(nameof(mapFunc));
 
-            foreach (var item in source)
+            foreach (var item in sourceList)
             {
                 mapFunc(item);
                 yield return item;
@@ -43,12 +48,15 @@ namespace Farrellsoft.UtilityFunctions
         public static IEnumerable<T> Intersection<T>(this IEnumerable<T> source, IEnumerable<T> other, IEqualityComparer<T> comparer = null)
         {
             if (source == null) throw new ArgumentNullException(nameof(source));
+            var sourceList = source.ToList();
+            
             if (other == null) throw new ArgumentNullException(nameof(other));
+            var otherList = other.ToList();
 
-            foreach (var item in source)
+            foreach (var item in sourceList)
             {
-                if (source.Contains(item, comparer ?? EqualityComparer<T>.Default) &&
-                    other.Contains(item, comparer ?? EqualityComparer<T>.Default))
+                if (sourceList.Contains(item, comparer ?? EqualityComparer<T>.Default) &&
+                    otherList.Contains(item, comparer ?? EqualityComparer<T>.Default))
                 {
                     yield return item;
                 }
@@ -58,16 +66,53 @@ namespace Farrellsoft.UtilityFunctions
         public static IEnumerable<T> Difference<T>(this IEnumerable<T> source, IEnumerable<T> other, IEqualityComparer<T> comparer = null)
         {
             if (source == null) throw new ArgumentNullException(nameof(source));
+            var sourceList = source.ToList();
+            
             if (other == null) throw new ArgumentNullException(nameof(other));
+            var otherList = other.ToList();
 
-            foreach (var item in source)
+            foreach (var item in sourceList)
             {
-                if (!other.Contains(item, comparer ?? EqualityComparer<T>.Default))
+                if (!otherList.Contains(item, comparer ?? EqualityComparer<T>.Default))
                 {
                     yield return item;
                 }
             }
         }
-        
+
+        public static IEnumerable<T> Product<T>(this IEnumerable<T> source, IEnumerable<T> other, IEqualityComparer<T> comparer = null)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+            var sourceList = source.ToList();
+            
+            if (other == null) throw new ArgumentNullException(nameof(other));
+            var otherList = other.ToList();
+            
+            var returnList = new List<T>(sourceList);
+            foreach (var item in otherList)
+            {
+                if (returnList.Contains(item, comparer ?? EqualityComparer<T>.Default) == false)
+                {
+                    returnList.Add(item);
+                }
+            }
+            
+            return returnList;
+        }
+
+        public static IEnumerable<T> Complement<T>(this IEnumerable<T> source, IEnumerable<T> other, IEqualityComparer<T> comparer = null)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+            var sourceList = source.ToList();
+            
+            if (other == null) throw new ArgumentNullException(nameof(other));
+            var otherList = other.ToList();
+            
+            foreach (var item in otherList)
+            {
+                if (sourceList.Contains(item, comparer ?? EqualityComparer<T>.Default) == false)
+                    yield return item;
+            }
+        }
     }
 }
